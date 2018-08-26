60852c79f3ac1a549e954f8506e8785a060cfa98
RAHUL AGARWAL <rahul89.agarwal@gmail.com>
2018-08-26 14:43:53 +0530

    added popover for skill description

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-$(document).ready({
+$(document).ready(function() {
     // add any custom jquery script in here
+    let popover = $('[data-toggle="popover"]').popover({
+        placement: 'top',
+        template: '<div class="popover" role="tooltip">'
+                +   '<div class="arrow"></div>'
+                +   '<h3 class="popover-header"></h3>'
+                +   '<div class="popover-body"></div>'
+                + '</div>',
+        trigger  : 'hover focus'
+    });
+    $('ul.languages > li').focusout(popover.popover('hide'));
+    $('ul.languages > li').mouseout(popover.popover('hide'));
+    $('ul.languages > li').mouseleave(popover.popover('hide'));
 });
